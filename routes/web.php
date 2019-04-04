e94c20c6a69078ef3a00205c50947081f1982152
m7md3omer <mohammedomer789@gmail.com>
2019-04-04 20:56:07 +0200

    using Eloquent to fetch data from the database and and added pagination and created the index layout for all post and the show layout for the specific post

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,11 @@
 
 // Route::get('/user/{id}/{name}', function($id, $n){
 //     return 'this is user '. $n .'with an id of '. $id;
-// }); 
+// });
 
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
-Route::resource('post', 'PostsController');
+// to include all methods and maping them to their respective
+Route::resource('posts', 'PostsController');
